9e179c46a9f3e391546f6af022f29ddd74c9da05
ciki6 <18694942447@163.com>
2022-03-25 21:47:04 +0800

    antd 按需加载

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: 陆晓夫
+ * @Date: 2022-03-22 10:32:35
+ * @LastEditors: 陆晓夫
+ * @LastEditTime: 2022-03-23 15:28:41
+ */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(), 
+    //ant-design-vue组件按需加载
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
+});
